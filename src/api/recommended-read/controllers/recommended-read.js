0688ccf5aae6d9dcb61fd84436dc649a079a6d83
Chukwu <chukwudaluay@gmail.com>
2023-07-16 13:42:23 +0100

    add author data to response (custom config) 🍜

--- src/api/recommended-read/controllers/recommended-read.js
+++ src/api/recommended-read/controllers/recommended-read.js
@@ -1,9 +1,41 @@
+// @ts-nocheck
 'use strict';
 
 /**
  * recommended-read controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+// const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::recommended-read.recommended-read');
+// module.exports = createCoreController('api::recommended-read.recommended-read');
+
+const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::recommended-read.recommended-read', ({ strapi }) => ({
+  async find(ctx) {
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+    const query = strapi.query('api::recommended-read.recommended-read');
+    await Promise.all(
+      data.map(async (item, index) => {
+        const foundItem = await query.findOne({
+          where: {
+            id: item.id,
+          },
+          populate: ['createdBy', 'updatedBy'],
+        });
+
+        data[index].attributes.createdBy = {
+          id: foundItem.createdBy.id,
+          firstname: foundItem.createdBy.firstname,
+          lastname: foundItem.createdBy.lastname,
+        };
+        data[index].attributes.updatedBy = {
+          id: foundItem.updatedBy.id,
+          firstname: foundItem.updatedBy.firstname,
+          lastname: foundItem.updatedBy.lastname,
+        };
+      })
+    );
+    return { data, meta };
+  },
+}));
