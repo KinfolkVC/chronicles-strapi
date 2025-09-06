d24852aa863654faddf9238187727bfd8e66595b
chukwu <chukwudaluay@gmail.com>
2025-09-06 13:13:58 +0100

    feat: 🎸 send emails on user sub and unsub, add email template

---
+++ src/api/new-subscriber-email-template/controllers/new-subscriber-email-template.ts
@@ -0,0 +1,7 @@
+/**
+ * new-subscriber-email-template controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::new-subscriber-email-template.new-subscriber-email-template');
---
+++ src/api/new-subscriber-email-template/services/new-subscriber-email-template.ts
@@ -0,0 +1,7 @@
+/**
+ * new-subscriber-email-template service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::new-subscriber-email-template.new-subscriber-email-template');
